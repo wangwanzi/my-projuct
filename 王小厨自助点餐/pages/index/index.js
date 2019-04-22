dd29fe9f9ecc3bbebc87591a02edbb98467a3bae
王宁 <694337881@qq.com>
2019-04-22 16:33:09 +0800

    wangxiaochuzizhudiancan

---
+++ 王小厨自助点餐/pages/index/index.js
@@ -0,0 +1,17 @@
+Page({
+  data: {
+    imgs: [
+        {url: '../../image/index/banner1.png'},
+        {url: '../../image/index/banner2.png'},
+        {url: '../../image/index/banner3.png'}
+    ],
+      indicatorDots:true,
+      autoplay:true,
+      interval:3000,
+      circular:true,
+      duration:1000,
+      beforColor:'white',
+      afterColor:'coral'
+  },
+
+})
